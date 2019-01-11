219b1474dcd9f2e6d2b276fbb191e84459db9e0a
mahesh <mahesh61437mahe@gmail.com>
2019-01-11 14:19:55 +0530

    FirstCommit

---
+++ notesproject/notes/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+# Create your models here.
+class notes(models.Model):
+	title = models.CharField(max_length=100)
+	content = models.TextField()
+	date_posted = models.DateTimeField(default=timezone.now)
+	author = models.ForeignKey(User,on_delete = models.CASCADE)
+
+	def __str__(self):
+		return self.title
+
+	def get_absolute_url(self):
+		return reverse('notes-detail',kwargs={'pk':self.pk})
+
+class notifications(models.Model):
+	 user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+	 notes_id = models.ForeignKey(notes,on_delete=models.CASCADE)
+	 
+
+
+
+
